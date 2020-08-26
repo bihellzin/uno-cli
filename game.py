65c40d905859763049a1de85544e202352ed33e8
bihellzin <gof2@cin.ufpe.br>
2020-08-25 22:01:09 -0300

    deck creationg working

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from cards import *
-from colors import *
+from random import shuffle
 
 
 class Game:
@@ -7,18 +7,45 @@ class Game:
     self.deck = []
     self.stack = []
 
-  
+
   def generate_deck(self):
-    pass
+    self.generate_colored_cards()
+    self.generate_uncolored_cards()
+    shuffle(self.deck)
 
 
   def generate_colored_cards(self):
-    colors = [BLUE, GREEN, RED, YELLOW]
-    card_types = ['number', 'draw_2', 'reverse', 'skip']
+    self.generate_number_cards()
+    self.generate_special_colored_cards()
 
 
   def generate_number_cards(self):
     numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    colors = [BLUE, GREEN, RED, YELLOW]
 
     for number in numbers:
-      pass
+      for color in colors:
+        if number == 0:
+          self.deck.append(NumberCard('number', color, number))
+
+        else:
+          self.deck.append(NumberCard('number', color, number))
+          self.deck.append(NumberCard('number', color, number))
+
+
+  def generate_special_colored_cards(self):
+    card_types = ['draw_2', 'reverse', 'skip']
+    colors = [BLUE, GREEN, RED, YELLOW]
+
+    for card_type in card_types:
+      for color in colors:
+        self.deck.append(ColoredCard(card_type, color))
+        self.deck.append(ColoredCard(card_type, color))
+
+
+  def generate_uncolored_cards(self):
+    card_types = ['wild', 'wild_draw_4']
+
+    for card_type in card_types:
+      for i in range(4):
+        self.deck.append(Card(card_type))
