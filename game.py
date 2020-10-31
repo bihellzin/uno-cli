4259870b84eb0da2187393e8b015ababdf236416
bihellzin <gof2@cin.ufpe.br>
2020-10-30 21:42:15 -0300

    adding more methods

--- game.py
+++ game.py
@@ -10,8 +10,36 @@ class Game:
 		self.deck = Deck()
 
 
-class Rules:
+	def playing(self):
+		while not has_winner:
+			for i in range(player_order):
+				self.play(player_order[i])
+
+				self.check_card_played()
+
+
+	def play(self, player: Player):
+		card = input("Play a card !\n")
+		self.player.deck.pop(int(card))
+
+
+	def check_card_played(self):
+		pass
+
+
+	# Rules
 	@staticmethod
-	def reverse_card_played(self, game: Game):
+	def reverse_card_played(game: Game):
 		if game.deck.top_of_stack_card.card_type == 'reverse':
 			self.player_order.reverse()
+
+
+	@staticmethod
+	def skip_card_played(game: Game):
+		if game.deck.top_of_stack_card.card_type == 'skip':
+			self.player_order.reverse()
+
+
+	@staticmethod
+	def has_winner(game: Game):
+		pass
